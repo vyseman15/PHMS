f7cc37a1279d63f7d43ddbf9dd1451a2cf0007b5
Peter Sassaman <peter.sassaman@mavs.uta.edu>
2014-04-30 19:27:02 -0500

    Revises contacts to look like view doctors.

--- PMHS/PersonalHealth/src/com/example/personalhealth/ViewContactsInformation.java
+++ PMHS/PersonalHealth/src/com/example/personalhealth/ViewContactsInformation.java
@@ -1,124 +1,37 @@
 package com.example.personalhealth;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Locale;
 
+import com.example.personalhealth.ViewDietInformation.StableArrayAdapter;
+
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
-import android.support.v4.app.NavUtils;
-import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
 
 
 
-
-import com.example.personalhealth.contactsDB.Row;
-
-public class ViewContactsInformation extends ActionBarActivity {
+public class ViewContactsInformation  extends Activity {
 
 	public final static String EXTRA_MESSAGE = "medical.app";
 	String Username;
 	
-	
-	/*
-	public void main_page_click(View view){
-		//This will bring you back to sign in page
-		Intent go_to_main_page_intent = new Intent(this, MainPage.class);
-		go_to_main_page_intent.putExtra(EXTRA_MESSAGE,Username);
-		startActivity(go_to_main_page_intent);
-		finish();
-	}
-*/	
-	public void update_diet_information(View view){
-		//This will bring you back to sign in page
-		Intent go_to_main_page_intent = new Intent(this, UpdateDietInformation.class);
-		go_to_main_page_intent.putExtra(EXTRA_MESSAGE,Username);
-		startActivity(go_to_main_page_intent);
+	public void update_doctor_info(View view){
+		//pass Username to the UpdateUserInformation class
+		Intent update_user_info_intent = new Intent(this, AddNewDoctor.class);
+		update_user_info_intent.putExtra(EXTRA_MESSAGE,Username);
+		startActivity(update_user_info_intent);
 		finish();
 	}
 	
-	//Set all of the user information in the textviews
-		public void get_textviews_for_user_information()
-		{
-			Row dietInfo;
-			long DAY_IN_MS = 1000 * 60 * 60 * 24;
-			
-			setContentView(R.layout.fragment_contacts_page);
-			contactsDB Db = new contactsDB(this);
-			
-			
-			String [] dataall=new String[7];
-			for(int i=0;i<7;i++)
-			{
-				long time = System.currentTimeMillis()-(i*60*60*24*1000);
-				Date day = new Date(time);
-				String date = new SimpleDateFormat("dd-MM-yyyy",Locale.US).format(day);
-				dietInfo = Db.rowNullConstructor();
-				dietInfo.Date = date;
-				if(Db.checkExists(Username, date)==1)
-				{
-					dietInfo = Db.getsingleHealthRow(Username, date);
-				}
-				String indivData="";
-			    String dataBreaker="";
-			    
-				//if(dietInfo.Username.equals("q"));
-				
-			    
-				dataBreaker=String.valueOf(dietInfo.Date);
-			    indivData=indivData.concat("Date: "+dataBreaker+"\n");
-			    dataBreaker=String.valueOf(dietInfo.Weight);
-			    indivData=indivData.concat("Weight: "+dataBreaker+"\n");
-			    dataBreaker=String.valueOf(dietInfo.Calories);
-			    indivData=indivData.concat("Calories: "+dataBreaker+"\n");
-			    dataBreaker=String.valueOf(dietInfo.BMI);
-			    indivData=indivData.concat("BMI:"+dataBreaker+"\n");
-			    dataBreaker=String.valueOf(dietInfo.Workout_Plan);
-			    indivData=indivData.concat("Workout Plan:"+dataBreaker+"\n");
-			    dataBreaker=String.valueOf(dietInfo.Calories_Burned);
-			    indivData=indivData.concat("Caloires Burned:"+dataBreaker+"\n");
-
-			    dataall[i]=indivData;
-			}
-		    
-			ListView listview = (ListView) findViewById(R.id.listview1);
-
-		    final ArrayList<String> list = new ArrayList<String>();
-		    for (int i = 0; i < dataall.length; ++i) 
-		    {
-		      list.add(dataall[i]);
-		    }
-		    final StableArrayAdapter adapter = new StableArrayAdapter(this,
-		        android.R.layout.simple_list_item_1, list);
-		    listview.setAdapter(adapter);
-			
-			//This will put all of the data in the Row class/ userInfo object, which contains a row query, onto the screen in textviews
-			TextView Username_textview = (TextView)findViewById(R.id.Username_id);
-			Username_textview.setText("Username: "+Username);
-			
-		    
-		}
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		getActionBar().setDisplayHomeAsUpEnabled(true);
-		//gets the username from the main page
-		Intent intent = getIntent();
-		Username = intent.getStringExtra(MainPage.EXTRA_MESSAGE);
-		get_textviews_for_user_information();
-	}
 	
 	
 	///This is the adapter for the string of data
@@ -147,30 +60,75 @@ public class ViewContactsInformation extends ActionBarActivity {
 
 	  }
 	
+	public void displayDoctorInformation()
+	{
+		int rowcount;
+		String Doctor_Name, Doctor_Email, Doctor_Type, Doctor_Phone;
+		Cursor c;
+		userInfoDB db = new userInfoDB(this);
+		c = db.getDoctorInformation(Username);
+		rowcount = c.getCount();
+		String [] dataall=new String[rowcount];
+		if ((c != null) && (c.moveToFirst()))
+		{
+			for(int i=0;i<rowcount;i++)
+			{
+				Doctor_Name = c.getString(c.getColumnIndex("Doctor_Name"));
+				Doctor_Email = c.getString(c.getColumnIndex("Doctor_Email"));
+				Doctor_Phone = c.getString(c.getColumnIndex("Doctor_Phone"));
+				Doctor_Type = c.getString(c.getColumnIndex("Doctor_Type"));
+				c.moveToNext();
+				
+				
+				String indivData="";
+			    String dataBreaker="";
+			    
+				//if(dietInfo.Username.equals("q"));
+				
+			    
+				dataBreaker=String.valueOf(Doctor_Name);
+			    indivData=indivData.concat("Doctor_Name: "+dataBreaker+"\n");
+			    dataBreaker=String.valueOf(Doctor_Email);
+			    indivData=indivData.concat("Doctor_Email: "+dataBreaker+"\n");
+			    dataBreaker=String.valueOf(Doctor_Phone);
+			    indivData=indivData.concat("Doctor_Phone: "+dataBreaker+"\n");
+			    dataBreaker=String.valueOf(Doctor_Type);
+			    indivData=indivData.concat("Doctor_Type: "+dataBreaker+"\n");
+	
+			    dataall[i]=indivData;
+				
+				
+			    ListView listview = (ListView) findViewById(R.id.listview1);
+				
+			    final ArrayList<String> list = new ArrayList<String>();
+			    for (int j = 0; j < dataall.length; ++j) 
+			    {
+			      list.add(dataall[j]);
+			    }
+			    final StableArrayAdapter adapter = new StableArrayAdapter(this,
+			        android.R.layout.simple_list_item_1, list);
+			    listview.setAdapter(adapter);
+				
+			}
+		}
+	}
+	
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_view_doctors);
+		Intent intent = getIntent();
+		Username = intent.getStringExtra(UserInformation.EXTRA_MESSAGE);
+		displayDoctorInformation();
+	}
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.medication__screen, menu);
-		return super.onCreateOptionsMenu(menu);
-	}
-	
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case android.R.id.home:
-			// This ID represents the Home or Up button. In the case of this
-			// activity, the Up button is shown. Use NavUtils to allow users
-			// to navigate up one level in the application structure. For
-			// more details, see the Navigation pattern on Android Design:
-			//
-			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
-			//
-			NavUtils.navigateUpFromSameTask(this);
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
+		getMenuInflater().inflate(R.menu.view_doctors, menu);
+		return true;
 	}
 
-
-	}
-	
+}
